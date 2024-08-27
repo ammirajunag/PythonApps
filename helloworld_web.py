8fc55fa0203e5d006addda8f17594d34cc5b889e
ammirajunag <41837254+ammirajunag@users.noreply.github.com>
2024-08-26 23:09:59 -0400

    added a for loop

--- helloworld_web.py
+++ helloworld_web.py
@@ -3,9 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+num = 0
+
 @app.route('/')
+for i in range(5):
+    num += 1
+    print(num)
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!' + str(num)
 
 if __name__ == '__main__':
     app.run(debug=True)
