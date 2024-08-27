0b640607da412f2fce11342aa201fb0f8857e41e
ammirajunag <41837254+ammirajunag@users.noreply.github.com>
2024-08-26 23:19:08 -0400

    moved hell world string into the for loop

--- helloworld_web.py
+++ helloworld_web.py
@@ -8,8 +8,9 @@ def hello_world():
     num = 0
     for i in range(5):
         num += 1
+        return 'Hello, World!' + str(num)
         print(num)
-    return 'Hello, World!' + str(num)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
