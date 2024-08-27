18b8876c87bae95d3fc700670ec0550974e9daab
ammirajunag <41837254+ammirajunag@users.noreply.github.com>
2024-08-26 23:13:11 -0400

    changed def helloworld code use for loop

--- helloworld_web.py
+++ helloworld_web.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 num = 0
 
 @app.route('/')
-for i in range(5):
-    num += 1
-    print(num)
 def hello_world():
+    for i in range(5):
+        num += 1
+        print(num)
     return 'Hello, World!' + str(num)
 
 if __name__ == '__main__':
